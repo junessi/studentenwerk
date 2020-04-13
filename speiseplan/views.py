2ec91d3848afefbc1cb54d171057b85860523ad0
Junlin Huang <cricketlong@gmail.com>
2020-04-13 14:22:34 +0000

    move like and unlike functionality to meal_info

--- speiseplan/views.py
+++ speiseplan/views.py
@@ -32,9 +32,9 @@ def canteen_date(request, canteen_id, date):
     return JsonResponse(requests.get(url).json(), safe = False)
 
 def canteen_meals(request, canteen_id, date):
-    url = base_url + "/canteens/" + canteen_id + "/days/" + date + "/meals/"
-
+    # url = base_url + "/canteens/" + canteen_id + "/days/" + date + "/meals/"
     # meals = requests.get(url).json()
+
     meals = [{ "id": 260,
                     "name": "Gemüse-Couscouspfanne mit Joghurt-Ingwer-Dip, dazu bunter Blattsalat",
                     "notes": ["ovo-lacto-vegetabil", "mensaVital"],
@@ -55,34 +55,58 @@ def canteen_meals(request, canteen_id, date):
     for m in meals:
         result = MealQuery().get_meal(m["id"])
         if len(result):
-            likes_count = len(array.array('L', result[0].liked_users).tolist())
-            m["likes"] = likes_count
+            liked_users = array.array('L', result[0].liked_users).tolist()
+            m["liked"] = False
+            m["likes"] = len(liked_users)
+            if "wechat_uid" in request.GET and int(request.GET["wechat_uid"]) in liked_users:
+                m["liked"] = True
         else:
             meal = Meal(m["id"])
             meal.save()
 
     return JsonResponse(meals, safe = False)
-    # return JsonResponse(meals, safe = False)
 
 def canteen_meal_detail(request, canteen_id, date, meal_id):
-    url = base_url + "/canteens/" + canteen_id + "/days/" + date + "/meals/" + meal_id
+    # url = base_url + "/canteens/" + canteen_id + "/days/" + date + "/meals/" + meal_id
+    # meal_info = requests.get(url).json()
 
-    return JsonResponse(requests.get(url).json(), safe = False)
+    meal_info = {"status": 404, "message": "meal not found"}
+    if int(meal_id) == 260:
+        meal_info = {
+                        "id": 260,
+                        "name": "Gemüse-Couscouspfanne mit Joghurt-Ingwer-Dip, dazu bunter Blattsalat",
+                        "notes": ["ovo-lacto-vegetabil", "mensaVital"],
+                        "prices": {"students": 2.3, "employees": 3.65, "others": 4.6},
+                        "category": "Alternativ-Angebot",
+                        "likes": 0
+                     }
 
-def likes(request, canteen_id, date, meal_id):
-    
+    elif int(meal_id) == 10900:
+        meal_info = {
+                        "id": 10900,
+                        "name": "Hähnchenschnitzel mit Brötchen",
+                        "notes": [],
+                        "prices": { "pupils": 2.4, "others": 4.3},
+                        "category": "Cafeteria Heiße Theke",
+                        "likes": 0
+                     }
+
+    meal_info["likes"] = 0
+    meal_info["liked"] = False
     wechat_uid = 0
     liked_users = []
     updated = False
     result = MealQuery().get_meal(meal_id)
 
     if len(result):
-        liked_users = array.array('L', result[0].liked_users).tolist()
+        meal = result[0]
+        liked_users = array.array('L', meal.liked_users).tolist()
 
         if "action" in request.GET and "wechat_uid" in request.GET:
             if request.GET["action"] == "like":
                 if int(request.GET["wechat_uid"]) not in liked_users:
                     liked_users.append(int(request.GET["wechat_uid"]))
+                    meal_info["liked"] = True
 
                     updated = True
             elif request.GET["action"] == "unlike":
@@ -92,8 +116,21 @@ def likes(request, canteen_id, date, meal_id):
                     updated = True
 
             if updated:
-                result[0].liked_users = array.array('L', liked_users)
-                result[0].update_data()
+                meal.liked_users = array.array('L', liked_users)
+                meal.update_data()
+
+        meal_info["likes"] = len(liked_users)
+
+    return JsonResponse(meal_info, safe = False)
+
+def likes(request, canteen_id, date, meal_id):
+    
+    liked_users = []
+    result = MealQuery().get_meal(meal_id)
+
+    if len(result):
+        meal = result[0]
+        liked_users = array.array('L', meal.liked_users).tolist()
 
     return JsonResponse({"status": 200, "liked_users": liked_users}, safe = False)
 
